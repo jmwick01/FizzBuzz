4b342bac94d65ca68a7d6e24e29a3b11e4cc79c4
Jason Wicker <jmwick01@gmail.com>
2024-03-13 09:05:31 -0400

    Updated JS
    Added JavaScript to the project. Created 3 CSS classes too to be assigned via JS.

--- js/site.js
+++ js/site.js
@@ -2,15 +2,20 @@
 function getValues() {
 
     //get the user values from the page
-
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
     //check for numbers
-
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
     //check that they are integers
-
-    //we call fizzBuzz
-
-    //call displayData and write the values to the screen
-
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
+        //we call fizzBuzz
+        let fbArray = fizzBuzz(fizzValue, buzzValue);
+        //call displayData and write the values to the screen
+        displayData(fbArray);
+    } else {
+        alert("You must enter integers");
+    }
 }
 
 
@@ -19,26 +24,58 @@ function fizzBuzz(fizzValue, buzzValue) {
     let returnArray = [];
 
     //loop from 1 to 100
-
-    //we need to check the current value in 3 steps
-    //check to see if divisible by both (3 and 5)
-    //if so, push "FizzBuzz" into an array and not the number
-
-    //check to see if divisible by fizz value (3)
-    //if so, push "Fizz" into an array and not the number
-
-    //check to see if divisible by buzz value (5)
-    //if so, push "Buzz" into an array and not the number
-
-    //if none then push the number into the array
-
-    returnArray;
+    for (let i = 1; i <= 100; i++) {
+        //check to see if divisible by both (3 and 5)
+        //check to see if divisible by fizz value (3)
+        //check to see if divisible by buzz value (5)
+        if (i % fizzValue == 0 && i % buzzValue == 0) {
+            returnArray.push("FizzBuzz");
+        } else if (i % fizzValue == 0) {
+            returnArray.push("Fizz");
+        } else if (i % buzzValue == 0) {
+            returnArray.push("Buzz");
+        } else {
+            returnArray.push(i);
+        }
+    }
+     return returnArray;
 }
 
-
+//loop over the array and create a tablerow for each item
 function displayData(fbArray) {
-    
-    //loop over the array and create a tablerow for each item.
+    //get the table body element from the page
+    let tableBody = document.getElementById("results");
+
+    //get the template row
+    let templateRow = document.getElementById("fbTemplate");
+
+    //clear table first
+    tableBody.innerHTML = "";
+
+    for (let index = 0; index < fbArray.length; index += 5) {
+        
+        let tableRow = document.importNode(templateRow.content, true);
+
+        //grab just the tds and put into an array
+        let rowCols = tableRow.querySelectorAll("td");
+
+        rowCols[0].classList.add(fbArray[index]);
+        rowCols[0].textContent = fbArray[index];
+        
+        rowCols[1].classList.add(fbArray[index+1]);
+        rowCols[1].textContent = fbArray[index+1];
+
+        rowCols[2].classList.add(fbArray[index+2]);
+        rowCols[2].textContent = fbArray[index+2];
+
+        rowCols[3].classList.add(fbArray[index+3]);
+        rowCols[3].textContent = fbArray[index+3];
+
+        rowCols[4].classList.add(fbArray[index+4]);
+        rowCols[4].textContent = fbArray[index+4];
+        
+        tableBody.appendChild(tableRow);
+    }
 
     //add all the rows to the table
 }
