913198da5ad0c23c9fe40d7d36d7a5048016517f
Jason Wicker <jmwick01@gmail.com>
2024-03-12 22:34:36 -0400

    Updated JS
    Added pseudo code to JS file

--- js/site.js
+++ js/site.js
@@ -1,4 +1,44 @@
-//Call Hello World
-function helloWorld() {
-    alert("Hello World");    
+//get values from the user. We need to get the fizz value and the buzz value.
+function getValues() {
+
+    //get the user values from the page
+
+    //check for numbers
+
+    //check that they are integers
+
+    //we call fizzBuzz
+
+    //call displayData and write the values to the screen
+
+}
+
+
+function fizzBuzz(fizzValue, buzzValue) {
+    
+    let returnArray = [];
+
+    //loop from 1 to 100
+
+    //we need to check the current value in 3 steps
+    //check to see if divisible by both (3 and 5)
+    //if so, push "FizzBuzz" into an array and not the number
+
+    //check to see if divisible by fizz value (3)
+    //if so, push "Fizz" into an array and not the number
+
+    //check to see if divisible by buzz value (5)
+    //if so, push "Buzz" into an array and not the number
+
+    //if none then push the number into the array
+
+    returnArray;
+}
+
+
+function displayData(fbArray) {
+    
+    //loop over the array and create a tablerow for each item.
+
+    //add all the rows to the table
 }
